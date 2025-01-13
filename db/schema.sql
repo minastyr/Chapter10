911ba265e0febd3382d5000b1456d04e7669d7ea
AnnuArchon <minastyr@gmail.com>
2025-01-12 20:03:01 -0500

    starting files

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE hr;
+
+\c hr;
+
+Create table department (
+    id serial primary key,
+    name varchar(30) not null
+);
+
+Create table employee (
+    id serial primary key,
+    first_name varchar(30) not null,
+    last_name varchar(30) not null,
+    role_id integer not null,
+    manager_id integer,
+    foreign key (manager_id) references employee (id)
+);
+
+Create table role (
+    id serial primary key,
+    title varchar(30) not null,
+    salary decimal not null,
+    department integer not null,
+    foreign key (department) references department (id)
+);
+
